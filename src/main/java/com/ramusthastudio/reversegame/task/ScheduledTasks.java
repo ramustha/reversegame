041a3cf643a081f628537594f4ba605b87afb681
ramustha <ramusthanest@gmail.com>
2017-03-30 11:28:45 +0000

    fixing controller

--- src/main/java/com/ramusthastudio/reversegame/task/ScheduledTasks.java
+++ src/main/java/com/ramusthastudio/reversegame/task/ScheduledTasks.java
@@ -96,13 +96,10 @@ public class ScheduledTasks {
             LOG.info("StartingGame.... Quest : {} Answer : {} level {} ", quest, answer, gameLevel);
             pushMessage(fChannelAccessToken, userId, quest);
             fDao.updateGameWord(new GameWord(userId, quest, answer, wordCount, gameLevel, currentTimeMillis(), 0));
-
-            if (wordCount > 0) {
-              LOG.info("User not answering....");
-              fDao.updateGameStatus(new GameStatus(userId, status, wordTrue, ++wordFalse, currentTimeMillis(), isAnswer));
-            }
-
+            fDao.updateGameStatus(new GameStatus(userId, status, wordTrue, ++wordFalse, currentTimeMillis(), isAnswer));
           }
+
+          fDao.updateGameStatus(new GameStatus(userId, status, wordTrue, wordFalse, currentTimeMillis(), false));
         }
       }
     }
